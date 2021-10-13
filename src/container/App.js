3ca851fb10ab7be1e0565c9fd2c8c52835bea7cb
Julien Richard <julien.richard@wildcodeschool.com>
2021-10-13 09:39:57 +0200

    mise en place de la structure du projet

---
+++ src/container/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Robots from '../data/robots';
+
+function App() {
+  return (
+    <div className="Container">
+      <h1 className="text-center text-primary">Profil Finder with react</h1>
+    </div>
+  );
+}
+
+export default App;
